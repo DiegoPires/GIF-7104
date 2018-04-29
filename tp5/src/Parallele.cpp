eb88da90bba3c26ab682689a395e0f04f9a3463d
Diego Silva Pires <diego_silva_pires@outlook.com>
2018-04-29 16:31:51 -0400

    Last tests

--- tp5/src/Parallele.cpp
+++ tp5/src/Parallele.cpp
@@ -25,55 +25,67 @@ Chrono executerParallele(int d, double seuil, int iterations, int coeur, const s
 
     int iterationCount = 1;
 
-    //omp_set_num_threads(coeur);
+    omp_set_num_threads(coeur);
 
     int part_rows, th_id;
     part_rows = d/coeur;
 
     Chrono lChrono(true);
 
-    //#pragma omp parallel
+    int oddLineStart = 1;
+    int evenLineStart = 1;
+    #pragma omp parallel shared (u) //reduction(+: iterationCount)
     while (!stopSeuil) {
 
-        #pragma omp parallel //shared(stopSeuil)
+        #pragma omp parallel for //shared(stopSeuil)
+
         for (int i=1; i < d - 1; i++) {
 
-            //#pragma omp barrier
-            //#pragma omp single
+            //pragma omp sections nowait
             //{
-                 int oddLineStart = i % 2 == 0 ? 1 : 2;
-                 int evenLineStart = i % 2 == 0 ? 2 : 1;
-            //}
-
+            
+                //#pragma omp parallel
             // Red sweep.
-            //#pragma omp for  //shared( d, originalU, stopSeuil, i)
-            #pragma omp parallel for //schedule(guided,part_rows)
-            for (int j = oddLineStart; j < d-1; j+=2)
-            {
-                u[i][j] = 0.25*(u[i][j-1]+u[i-1][j]+u[i][j+1]+u[i+1][j]);
+                //#pragma omp section
+                //{
+                    oddLineStart = i % 2 == 0 ? 1 : 2;
 
-                if ( u[i][j] <= seuil)
-                {
-                    stopSeuil = true;
-                }
-            }
+                    //#pragma omp for  //shared( d, originalU, stopSeuil, i)
+                    //#pragma omp parallel for //schedule(static,part_rows) shared(d, stopSeuil, i) //dynamic cest mauvais
+                    for (int j = oddLineStart; j < d-1; j+=2)
+                    {
+                        u[i][j] = 0.25*(u[i][j-1]+u[i-1][j]+u[i][j+1]+u[i+1][j]);
 
-            // Black sweep.
-            //#pragma omp for  //shared(u, d, originalU, stopSeuil, i)
-            #pragma omp parallel for //schedule(guided,part_rows)
-            for (int j = evenLineStart; j < d-1; j+=2)
-            {
-                u[i][j] = 0.25*(u[i][j-1]+u[i-1][j]+u[i][j+1]+u[i+1][j]);
+                        if ( u[i][j] <= seuil)
+                        {
+                            stopSeuil = true;
+                        }
+                    }
+                //}
 
-                if (u[i][j]  <= seuil)
-                {
-                    stopSeuil = true;
-                }
-            }
+                // Black sweep.
+                //#pragma omp section
+                //{
+                    evenLineStart = i % 2 == 0 ? 2 : 1;
+
+                    //#pragma omp for  //shared(u, d, originalU, stopSeuil, i)
+                    //#pragma omp parallel for //schedule(static,part_rows) shared(d, stopSeuil, i)
+                    for (int j = evenLineStart; j < d-1; j+=2)
+                    {
+                        u[i][j] = 0.25*(u[i][j-1]+u[i-1][j]+u[i][j+1]+u[i+1][j]);
+
+                        if (u[i][j]  <= seuil)
+                        {
+                            stopSeuil = true;
+                        }
+                    }
+                //}
+            //}
         }
 
         lChrono.pause();
 
+        //#pragma omp barrier
         if (iterations != 0 && iterationCount % iterations == 0 && !performanceTest) {
             string filename = "src/output/parallel/file_" + std::to_string(iterationCount) + ".bmp";
             print_output(u, d, &filename[0u]);
@@ -81,6 +93,7 @@ Chrono executerParallele(int d, double seuil, int iterations, int coeur, const s
 
         lChrono.resume();
 
+        //#pragma omp barrier
         iterationCount++;
     }
 
@@ -91,7 +104,7 @@ Chrono executerParallele(int d, double seuil, int iterations, int coeur, const s
     }
 
     if (iterations == 0 && !performanceTest) {
-        string filename = "src/output/parallele/file_0.bmp";
+        string filename = "src/output/parallel/file_0.bmp";
 
         cout << "Resultat final parallele\n";
         print_output(u, d, &filename[0u]);
